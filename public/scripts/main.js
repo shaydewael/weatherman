56b2a01f401c7cb8b727a3a524da0a600374d1eb
Caitlin Kennedy <kenned67@purdue.edu>
2016-04-17 16:13:27 -0400

    make object move up if space pressed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,63 +1,81 @@
 (function() {
-	"use strict";
-	var settings;
-	
-	//This is the game object
-	var WeatherMan = {
-		//Game canvas
-		canvas: 0,
-		ctx: 0,
-		//Boundaries
-		leftWall: 0,
-		rightWall: 0,
-		topWall: 0,
-		bottomWall: 0,
-		//Settings
-		settings: {
-			//For canvas
-			width: "850px",
-			height: "500px",
-			// Weather location
-			location: 0
-		},
-		init: function() {
-                        this.initListeners();
-			settings = this.settings;
-			WeatherMan.canvas = document.querySelector('canvas');
-			WeatherMan.ctx = WeatherMan.canvas.getContext("2d");
-			//Enter gameLoop
-			var gameLoop = WeatherMan.startLoop();
-		},
-                jump: function() {
-                    console.log('Recognize jump');
-                },
-                initListeners: function() {
-                        window.addEventListener('keypress', function(e) {
-                                var key = e.keyCode;
-                                
-                                switch(key) {
-                                      case 32:
-                                          e.preventDefault();
-                                          WeatherMan.jump();
+  "use strict";
+  var settings;
 
-                                }
-                        }, false);
-                },
-		startLoop: function() {
-			//Start game loop, draw objects and stuff
+  var man = {
+      height: 100,
+      width: 50,
+      x: 50,
+      y: 400,
 
-			//We won't use this parth, it's just to show how to draw circle :)
-			WeatherMan.ctx.clearRect(0, 0, WeatherMan.canvas.width, WeatherMan.canvas.height);
-			WeatherMan.ctx.beginPath();
-			WeatherMan.ctx.arc(100, 100, 30, 0, Math.PI*2, true);
-			WeatherMan.ctx.fillStyle = "#3498db";
-			WeatherMan.ctx.fill();
-			WeatherMan.ctx.closePath();
-			//End non-use part
-		}
-	};
-	window.onload = function() {
-		//This is the main method
-		WeatherMan.init();
-	};
+    drawMan: function(e) {
+      console.log(man.y);
+      WeatherMan.ctx.clearRect(0, 0, WeatherMan.canvas.width, WeatherMan.canvas.height);
+      WeatherMan.ctx.beginPath();
+      WeatherMan.ctx.rect(man.x, man.y, man.width, man.height);
+      //WeatherMan.ctx.fillStyle = "#3498db";
+      WeatherMan.ctx.stroke();
+      WeatherMan.ctx.closePath();
+    },
+    jump: function() {
+          console.log("jump");
+          if (man.y > 0) {
+            man.y -= 50;
+          }
+          man.drawMan();
+          }
+  }
+
+  //This is the game object
+  var WeatherMan = {
+    //Game canvas
+    canvas: 0,
+    ctx: 0,
+    //Boundaries
+    leftWall: 0,
+    rightWall: 0,
+    topWall: 0,
+    bottomWall: 0,
+    //Settings
+    settings: {
+      //For canvas
+      width: "850px",
+      height: "500px",
+      // Weather location
+      location: 0
+    },
+    init: function() {
+            this.initListeners();
+            settings = this.settings;
+            WeatherMan.canvas = document.querySelector('canvas');
+            WeatherMan.ctx = WeatherMan.canvas.getContext("2d");
+            //Enter gameLoop
+            var gameLoop = WeatherMan.startLoop();
+          },
+    initListeners: function() {
+                     window.addEventListener('keypress', function(e) {
+                       var key = e.keyCode;
+
+                       switch(key) {
+                         case 32:
+                           e.preventDefault();
+                           man.jump();
+
+                       }
+                     }, false);
+                   },
+    startLoop: function() {
+            this.interval = setInterval(function() {
+              WeatherMan.loop();
+            }, 1000/25);
+           },
+    startLoop: function() {
+                 //Start game loop, draw objects and stuff
+                 man.drawMan();
+               }
+  };
+  window.onload = function() {
+    //This is the main method
+    WeatherMan.init();
+  };
 })();
