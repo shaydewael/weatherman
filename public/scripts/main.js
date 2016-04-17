dbf84e9b217652f03bc20bb319c69c59094d14cf
Caitlin Kennedy <kenned67@purdue.edu>
2016-04-17 18:05:20 -0400

    add coins and score

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,41 @@
 (function() {
   "use strict";
   var settings;
-
+  
+  var coin = {
+    radius: 20,
+    y: 400,
+    x: 850,
+    velocity: 4,
+    
+    drawCoin: function(e) {
+      WeatherMan.ctx.beginPath();
+      WeatherMan.ctx.arc(coin.x, coin.y, coin.radius, 0, 2*Math.PI);
+      WeatherMan.ctx.fillStyle = "#ffff00";
+      WeatherMan.ctx.fill();
+      WeatherMan.ctx.closePath();
+    }
+  }
+  var obstacle = {
+    height: 25,
+    width: 25,
+    y: 475,
+    x: 850,
+    velocity: 10,
+    acceleration: .05,
+  
+    drawObstacle: function(e) {
+      WeatherMan.ctx.beginPath();
+      WeatherMan.ctx.rect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+      WeatherMan.ctx.fillStyle = "#000066";
+      WeatherMan.ctx.fill();
+      WeatherMan.ctx.closePath();
+    }
+  }
   var man = {
       height: 100,
       width: 50,
-      x: 50,
+      x: 100,
       y: 400,
       inAir: 0,
       gravity: 4,
@@ -15,8 +45,8 @@
       WeatherMan.ctx.clearRect(0, 0, WeatherMan.canvas.width, WeatherMan.canvas.height);
       WeatherMan.ctx.beginPath();
       WeatherMan.ctx.rect(man.x, man.y, man.width, man.height);
-      //WeatherMan.ctx.fillStyle = "#3498db";
-      WeatherMan.ctx.stroke();
+      WeatherMan.ctx.fillStyle = "#3498db";
+      WeatherMan.ctx.fill();
       WeatherMan.ctx.closePath();
     },
     jump: function() {
@@ -37,6 +67,7 @@
     rightWall: 0,
     topWall: 0,
     bottomWall: 0,
+    score: 0,
     //Settings
     settings: {
       //For canvas
@@ -84,7 +115,25 @@
                     }
                  }
                  man.drawMan();
-               }
+                 
+                 obstacle.velocity = obstacle.velocity + obstacle.acceleration;
+                 obstacle.x = obstacle.x - obstacle.velocity;
+                 if (obstacle.x < 0 ) {
+                  obstacle.x = 850;
+                 }
+                 if (obstacle.velocity > 60) {
+                    obstacle.acceleration = 0; 
+                  }
+                 obstacle.drawObstacle();
+
+                 coin.x = coin.x - coin.velocity;
+                 if (coin.x < 150) {
+                   coin.x = 850;
+                   WeatherMan.score++;
+                   console.log(WeatherMan.score);
+                }
+                coin.drawCoin();
+      }
   };
   window.onload = function() {
     //This is the main method
